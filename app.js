9450d6e296ee68f764f036beea649ab0712da2f1
Abraham Dio Suharli <abraham.suharli@salesforce.com>
2019-05-02 21:25:18 -0700

    try to add amp headers

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const sampleJson = {
         {a: 123}
     ]
 }
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("access-control-expose-headers", "AMP-Access-Control-Allow-Source-Origin, AMP-Redirect-To");
+    res.header("amp-access-control-allow-source-origin", "amp@gmail.dev");
+    next();
+});
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/timeslot', (req, res) => res.json(sampleJson))
 app.get('/increase', (req, res) => {
