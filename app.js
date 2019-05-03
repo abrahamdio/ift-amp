19911112788273a11cb97c6f97897294a15a7a25
Abraham Dio Suharli <abraham.suharli@salesforce.com>
2019-05-02 21:09:07 -0700

    init

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const sampleJson = {
+    items: [
+        {a: 123}
+    ]
+}
+
+app.get('/timeslot', (req, res) => res.json(sampleJson))
+app.get('/increase', (req, res) => {
+    sampleJson.items.push({ b: 123 });
+    res.json(sampleJson);
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
